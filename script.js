d5ed784268fd5d465884284da041ea370add3a02
furenku <furenku@gmail.com>
2021-05-31 22:06:42 -0500

    crear json con resultados

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
+/*
+The MIT License (MIT)
+
+Copyright © 2021 centroculturadigital.mx
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
 function pad(d) {
     return (d < 10) ? '0' + d.toString() : d.toString();
 }
 
+const fs = require("fs")
 const fetch = require("cross-fetch")
 
 const API_URL_ESTADOS = "https://gaia.inegi.org.mx/wscatgeo/mgem"
 const API_URL_MUNICIPIOS = "https://gaia.inegi.org.mx/wscatgeo/mgee"
 
 
-const obtenerMunicipios = (estados) => {
+const obtenerMunicipios = (estados, accion) => {
         
 
     try {
@@ -40,8 +54,11 @@ const obtenerMunicipios = (estados) => {
 
 
         Promise.all(municipios).then(m=>{
+
             municipios = m
-            console.log("estados", estados)
+
+            accion()
+
         }).catch(console.log)
 
 
@@ -80,7 +97,11 @@ const obtenerEstados = () => {
 
         estados = es
 
-        obtenerMunicipios( estados )
+        obtenerMunicipios( estados, () => {
+            
+            fs.writeFileSync('./resultado.json', JSON.stringify(estados));
+
+        })
 
     }).catch(console.log)
 
